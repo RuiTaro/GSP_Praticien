bee9ca9a6d7363582a758a8e925049d854206f98
radpavi <32639983+radpavi@users.noreply.github.com>
2017-11-13 16:59:03 +0100

    création de la page ''c_Connexion.php"

---
+++ controleurs/c_Connexion.php
@@ -0,0 +1,33 @@
+<?php
+$action = $_REQUEST['action']; //récupération de l'action
+//echo "action:".$action;
+
+switch($action)
+{ 
+	case 'verifConnexion': 
+		$login=Utilisateur::VerifUser($_POST['login'], $_POST['mdp']);
+		if (!empty($login)) 
+        {
+             $_SESSION['connexion']=$login;
+             header("refresh: 0;url=index.php");
+
+        }
+		
+		else 
+		{
+			header("refresh: 0;url=index.php?uc=administrer&action=connexion");
+		}
+		break;
+
+    case 'connexion':
+    	include("vues/v_connexion.php");
+    	break;
+
+    case 'deconnexion':
+    unset($_SESSION['connexion']);
+    	header("refresh: 0;url=index.php?uc=administrer&action=connexion");
+    	break;
+
+	default:echo "rien";
+}
+?>
