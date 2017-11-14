20fb61befdce602c07cdeff38b9a4d6f4d57c6e3
lexylievin <32639798+lexylievin@users.noreply.github.com>
2017-11-14 08:16:27 +0100

    affiche

--- vues/v_praticien.php
+++ vues/v_praticien.php
@@ -1,51 +1,236 @@
 <div id="page">
 	<div id="content">
 		<div class="box">
-			<h2>Liste des artistes</h2>
+			<h2>Liste des praticiens</h2>
 			<section>
-			<?php		
-			If (!empty( $_SESSION['connexion'])) // si quelqu'un est connecté
-			{ ?>
-				<a class="btn" href='index.php?uc=Artistes&action=ajouter'>Ajouter un artiste</a>
-			<?php } ?>
-			<table><tr><th>Numéro</th><th>Nom</th><th>Actions</th></tr>
-			<script>
-			function supprArtiste(id) 
-			{
-				if(confirm("Voulez vous vraimer supprimer cet artiste. Attention la suppression de l'artiste entrainera la suppression de tous ses albums ?"))
-				{
-					location.href='index.php?uc=Artistes&action=supprimer&numart='+id;
-				}
-				else {
-					alert("L'artiste n'a pas été supprimé.");
-				}
-			}
-			</script>
-			<?php
-			foreach($lesArtistes as $Artiste) //parcours du tableau d'objets récupérés
-			{ 	
-				$idArt=$Artiste->getId();           
-				$nom=$Artiste ->getNom();?>
-			<tr>
-			<td width=5%><?php echo $idArt?></td><td width=80%><?php echo $nom?></a></td><!--affichage dans des liens-->
-			<td class='action' width=15%>
-				<a href='index.php?uc=Albums&action=artiste&numart=<?php echo $idArt ?>' class="imageRechercher" title='Voir la liste des albums'></a> 
-				<?php		
-				If (!empty( $_SESSION['connexion']))  
-				{ ?>	
-					<a href='index.php?uc=Artistes&action=modifier&numart=<?php echo $idArt; ?>' class="imageModifier" title="modifier l'artiste"></a>
-					<span class="imageSupprimer" onclick="javascript:supprArtiste('<?php echo $idArt; ?>')" title="supprimer l'artiste" ></span> <!-- on met un span pour pouvoir invoquer le on click -->
-				<?php } ?>
-			</td>
-			</tr>
+			<div class="container">
 
-			<?php
-				}
-			?>
-			</table>
+	<ul class="tabs">
+		<li class="tab-link current" data-tab="tab-1">Praticien</li>
+		<li class="tab-link" data-tab="tab-2">Type praticien</li>
+		<li class="tab-link" data-tab="tab-3">Spécialité</li>
+		<li class="tab-link" data-tab="tab-4">Info</li>
+	</ul>
+
+	<div id="tab-1" class="tab-content current">
+	<table class="responstable">
+  
+  <tr>
+   <th>Sélection</th>
+    <th data-th="Driver details"><span>Nom</span></th>
+    <th>Adresse</th>
+    <th>Code postale</th>
+    <th>Ville</th>
+    <th>Coefficient</th>
+    <th>Type</th>
+    <th>Scpécialité</th>
+    <th>Options</th>
+  </tr>
+  
+  <tr>
+    <td><input type="radio"/></td>
+    <td>Steve</td>
+    <td>Foo</td>
+    <td>01/01/1978</td>
+    <td>Policyholder</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  
+  <tr>
+    <td><input type="radio"/></td>
+    <td>Steffie</td>
+    <td>Foo</td>
+    <td>01/01/1978</td>
+    <td>Spouse</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  
+  <tr>
+    <td><input type="radio"/></td>
+    <td>Stan</td>
+    <td>Foo</td>
+    <td>01/01/1994</td>
+    <td>Son</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  
+  <tr>
+    <td><input type="radio"/></td>
+    <td>Stella</td>
+    <td>Foo</td>
+    <td>01/01/1992</td>
+    <td>Daughter</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  
+</table>
+	</div>
+	<div id="tab-2" class="tab-content">
+		<table class="responstable">
+  
+  <tr>
+    <th>Sélection</th>
+    <th data-th="Driver details"><span>Nom</span></th>
+    <th>Adresse</th>
+    <th>Code postale</th>
+    <th>Ville</th>
+    <th>Coefficient</th>
+    <th>Type</th>
+    <th>Scpécialité</th>
+    <th>Options</th>
+  </tr>
+  
+  <tr>
+    <td><input type="radio"/></td>
+    <td>Steve</td>
+    <td>Foo</td>
+    <td>01/01/1978</td>
+    <td>Policyholder</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  
+  <tr>
+    <td><input type="radio"/></td>
+    <td>Steffie</td>
+    <td>Foo</td>
+    <td>01/01/1978</td>
+    <td>Spouse</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  
+  <tr>
+    <td><input type="radio"/></td>
+    <td>Stan</td>
+    <td>Foo</td>
+    <td>01/01/1994</td>
+    <td>Son</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  
+  <tr>
+    <td><input type="radio"/></td>
+    <td>Stella</td>
+    <td>Foo</td>
+    <td>01/01/1992</td>
+    <td>Daughter</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  
+</table>
+	</div>
+	<div id="tab-3" class="tab-content">
+	<table class="responstable">
+  
+  <tr>
+    <th>Sélection</th>
+    <th data-th="Driver details"><span>Nom</span></th>
+    <th>Adresse</th>
+    <th>Code postale</th>
+    <th>Ville</th>
+    <th>Coefficient</th>
+    <th>Type</th>
+    <th>Scpécialité</th>
+    <th>Options</th>
+  </tr>
+  
+  <tr>
+    <td><input type="radio"/></td>
+    <td>Steve</td>
+    <td>Foo</td>
+    <td>01/01/1978</td>
+    <td>Policyholder</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  
+  <tr>
+    <td><input type="radio"/></td>
+    <td>Steffie</td>
+    <td>Foo</td>
+    <td>01/01/1978</td>
+    <td>Spouse</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  
+  <tr>
+    <td><input type="radio"/></td>
+    <td>Stan</td>
+    <td>Foo</td>
+    <td>01/01/1994</td>
+    <td>Son</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  
+  <tr>
+    <td><input type="radio"/></td>
+    <td>Stella</td>
+    <td>Foo</td>
+    <td>01/01/1992</td>
+    <td>Daughter</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  
+</table>
+	</div>
+	<div id="tab-4" class="tab-content">
+		Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	</div>
+
+</div><!-- container -->
 			</section>
 		</div>
 	</div>
 	<br class="clearfix" />
 </div>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+	
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+})
+</script>
 
