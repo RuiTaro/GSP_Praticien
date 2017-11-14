0ec5cda5df8a5cb2f3eab4e5173aaac95fd42e99
lexylievin <32639798+lexylievin@users.noreply.github.com>
2017-11-14 18:10:50 +0100

    menu

--- vues/v_menuCR.php
+++ vues/v_menuCR.php
@@ -1,23 +1,36 @@
-<html>
-<head><title>GSB : Suivi de la Visite médicale </title></head>
 <body bgcolor="white" text="5599EE">
 <div name="haut" style="margin: 2 2 2 2 ;height:6%;"><h1><img src="images/logo.jpg" width="100" height="60"/></div>
 <div name="gauche" style="float:left;width:18%; background-color:white; height:100%;">
-	<h5>Outils</h5>
-	<ul>
-		<li><a href="">Comptes-Rendus</a></li>
-	</ul>
-			
+
+	<div id="menu">
 		<ul>
-			<li><a href="" >Nouveaux</a></li>
-			<li><a href="" >Médicaments</a></li>
-			<li><a href="index.php?uc=Praticiens&action=all" >Praticiens</a></li>
-			<li><a href="" >Autres visiteurs</a></li>
+			<li class="first">
+			<span class="opener">Comptes-rendus<b></b></span>
+					<ul>
+						<li><a href="">Voir les comptes-rendus</a></li>
+						<li><a href="">Nouveau</a></li>
+					</ul>
+			</li>
+			<li class="first">
+				<span class="opener">Médicaments<b></b></span>
+					<ul>
+						<li><a href="">Liste des medicaments</a></li>
+					</ul>
+			</li>
+			<li class="first">
+				<span class="opener">Praticiens<b></b></span>
+					<ul>
+						<li><a href="index.php?uc=Praticiens&action=all" >Praticiens</a></li>
+					</ul>
+			</li>
+			<li class="first">
+				<span class="opener">Visiteurs<b></b></span>
+					<ul>
+						<li><a href="">Liste des visiteurs</a></li>
+					</ul>
+			</li>
 		</ul>
-	</ul>
-</div>
-<div name="droite" style="float:left;width:80%;">
-	<div name="bas" style="margin : 10 2 2 2;clear:left;background-color:77AADD;color:white;height:88%;"></div>
-</div>
+		<br class="clearfix" />
+	</div>		
+	
 </body>
-</html>
