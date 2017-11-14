621cdc207fe404b0f1f060dfc7ea6105ebc6b374
radpavi <32639983+radpavi@users.noreply.github.com>
2017-11-14 19:18:05 +0100

    meta

--- vues/v_menuCR.php
+++ vues/v_menuCR.php
@@ -1,4 +1,5 @@
 <body bgcolor="white" text="5599EE">
+
 <div name="haut" style="margin: 2 2 2 2 ;height:6%;"><h1><img src="images/logo.jpg" width="100" height="60"/></div>
 <div name="gauche" style="float:left;width:auto; background-color:transparent; height:100%;">
 
@@ -8,7 +9,7 @@
 			<span class="opener">Comptes-rendus</span>					
 			</li>
 			<li class="first">
-				<span class="opener">Médicaments</span>					
+				<span class="opener">Medicaments</span>					
 			</li>
 			<li class="first">
 				<span class="opener"><a href="index.php?uc=Praticiens&action=all" >Praticiens</a></span>					
