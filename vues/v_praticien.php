cb96391d61362865fb39a743907d70f70e1e38f9
lexylievin <32639798+lexylievin@users.noreply.github.com>
2017-11-15 08:28:36 +0100

    tableau

--- vues/v_praticien.php
+++ vues/v_praticien.php
@@ -3,89 +3,101 @@
         
 		<div class="box">
 			<h2>Liste des praticiens</h2>
-			<section>
 			<div class="container">
 
-			<ul class="tabs">
-				<li class="tab-link current" data-tab="tab-1">Praticiens</li>
-        		<li class="tab-link" data-tab="tab-2">Spécialité</li>
-				<li class="tab-link" data-tab="tab-3">Type praticien</li>
-				<li class="tab-link" data-tab="tab-4">Info</li>
-			</ul>
+				<ul class="tabs">
+					<li class="tab-link current" data-tab="tab-1">Praticien</li>
+					<li class="tab-link" data-tab="tab-2">Type praticien</li>
+					<li class="tab-link" data-tab="tab-3">Spécialité</li>		
+				</ul>
 
-			<div id="tab-1" class="tab-content current">
-			<table class="responstable">
-  
- 			 <tr>
-	   			<th>Sélection</th>
-	    		<th data-th="Driver details"><span>Num</span></th>
-	    		<th>Nom</th>
-			    <th>Adresse</th>
-			    <th>Code postale</th>
-			    <th>Ville</th>
-			    <th>Coefficient</th>
-			    <th>Type</th>
-			    <th>Scpécialité</th>
-			    <th>Options</th>
-		  	</tr>
-	<?php
-  		foreach($lesPraticiens as $praticien) //parcours du tableau d'objets récupérés
-            {   
-                $idPrat=$praticien->getId();           
-                $nom=$praticien ->getNom();
-                $adresse=$praticien->getAdresse();           
-                $CP=$praticien ->getCP();
-                $ville=$praticien ->getVille();
-                $coef=$praticien->getCoef();
-	?>
+				<div id="tab-1" class="tab-content current">
+					<table class="responstable">
+				  
+					  	<tr>
+						   	<th>Sélection</th>
+						    <th data-th="Driver details"><span>Nom</span></th>
+						    <th>Adresse</th>
+						    <th>Code postale</th>
+						    <th>Ville</th>
+						    <th>Coefficient</th>
+						    <th>Type</th>
+						    <th>Scpécialité</th>
+						    <th>Options</th>
+					  	</tr>
+				  
+				  		<tr>
+						    <td><input type="radio"/></td>
+						    <td></td>
+						    <td></td>
+						    <td></td>
+						    <td></td>
+						    <td></td>
+						    <td></td>
+						    <td></td>
+						    <td></td>
+				  		</tr>  
+					</table>	
+				</div>
 
-    		<tr>
-            	<td input type="radio"><?php echo $idPrat?></td><td input type="radio"><?php echo $nom?></td><td input type="radio"><?php echo $adresse?></td><td input type="radio"><?php echo $CP?></td><td input type="radio"><?php echo $ville?></td><td input type="radio"><?php echo $coef?></td><!--affichage dans des liens-->
-  			</tr>
-	</table>
+				<div id="tab-2" class="tab-content">
+					<table class="responstable"> 
+					  	<tr>
+						    <th>Sélection</th>
+						    <th data-th="Driver details"><span>Nom</span></th>
+						    <th>Adresse</th>
+						    <th>Code postale</th>
+						    <th>Ville</th>
+						    <th>Coefficient</th>
+						    <th>Type</th>
+						    <th>Scpécialité</th>
+						    <th>Options</th>
+					  	</tr>
+				  
+					  	<tr>
+						    <td><input type="radio"/></td>
+						    <td></td>
+						    <td></td>
+						    <td></td>
+						    <td></td>
+						    <td></td>
+						    <td></td>
+						    <td></td>
+						    <td></td>
+					  	</tr>  
+					</table>
+				</div>
 
-	</div>
-	
-
-	</div><!-- container -->
-
-	<script type="text/javascript">
-		$(document).ready(function(){
-	
-<<<<<<< HEAD
-<<<<<<< HEAD
-		$('ul.tabs li').click(function(){
-=======
-	$('ul.tabs li').click(function()
-    {
->>>>>>> 8fd46483ef01476753a4f7c4dd2f36aa2f13f768
-=======
-	$('ul.tabs li').click(function()
-    {
->>>>>>> 8fd46483ef01476753a4f7c4dd2f36aa2f13f768
-		var tab_id = $(this).attr('data-tab');
-
-		$('ul.tabs li').removeClass('current');
-		$('.tab-content').removeClass('current');
-
-		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
-		})
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-	})
-	</script>
-=======
-    })
-    </script>
->>>>>>> 8fd46483ef01476753a4f7c4dd2f36aa2f13f768
-=======
-    })
-    </script>
->>>>>>> 8fd46483ef01476753a4f7c4dd2f36aa2f13f768
-			</section>
+				<div id="tab-3" class="tab-content">
+					<table class="responstable">
+			  
+					  <tr>
+					    <th>Sélection</th>
+					    <th data-th="Driver details"><span>Nom</span></th>
+					    <th>Adresse</th>
+					    <th>Code postale</th>
+					    <th>Ville</th>
+					    <th>Coefficient</th>
+					    <th>Type</th>
+					    <th>Scpécialité</th>
+					    <th>Options</th>
+					  </tr>
+			  
+					  <tr>
+					    <td><input type="radio"/></td>
+					    <td></td>
+					    <td></td>
+					    <td></td>
+					    <td></td>
+					    <td></td>
+					    <td></td>
+					    <td></td>
+					    <td></td>
+					  </tr> 
+					</table>
+				</div>
+			</div><!-- container -->	
 		</div>
 	</div>
 	<br class="clearfix" />
-</div>
+</div>    		
