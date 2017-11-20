44fb45c702df701858a20442225ab4d1594ca63d
lexylievin <32639798+lexylievin@users.noreply.github.com>
2017-11-20 13:31:58 +0100

    ajout de javascript

--- vues/v_praticien.php
+++ vues/v_praticien.php
@@ -121,4 +121,19 @@
 		</div>
 	</div>
 	<br class="clearfix" />
+	<script type="text/javascript">
+	$(document).ready(function(){
+	
+		$('ul.tabs li').click(function(){
+			var tab_id = $(this).attr('data-tab');
+
+			$('ul.tabs li').removeClass('current');
+			$('.tab-content').removeClass('current');
+
+			$(this).addClass('current');
+			$("#"+tab_id).addClass('current');
+		})
+
+	})
+	</script>
 </div>    		
