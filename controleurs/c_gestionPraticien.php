b3c668776acfadfdc70e16cdfd1095b3a3711bd1
lexylievin <32639798+lexylievin@users.noreply.github.com>
2017-11-20 16:15:32 +0100

    type

--- controleurs/c_gestionPraticien.php
+++ controleurs/c_gestionPraticien.php
@@ -2,6 +2,7 @@
 include 'vues/v_headerTab.php';
 include 'controleurs/c_Praticiens.php';
 include 'controleurs/c_Specialite.php';
+include 'controleurs/c_TypePraticiens.php';
 include 'vues/v_footerTab.php';
 
 ?>
