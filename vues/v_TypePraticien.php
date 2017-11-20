b347ab63385bb5e9749c3f69e82f86ebd41d37cf
radpavi <32639983+radpavi@users.noreply.github.com>
2017-11-20 16:12:47 +0100

    vueTypePrat

--- vues/v_TypePraticien.php
+++ vues/v_TypePraticien.php
@@ -1,29 +1,34 @@
 <div id="tabs-3" class="tab-content">
-	<table class="responstable">
+	<table class="responstable"> 
+	  	<tr>
+		    <th>Sélection</th>
+		    <th data-th="Driver details"><span>Numéro</span></th>
+            <th>Libelle</th>
+		    <th>Options</th>
+	  	</tr>
+	  	
 
-	  <tr>
-	    <th>Sélection</th>
-	    <th data-th="Driver details"><span>Numéro</span></th>
-        <th>Nom</th>
-	    <th>Adresse</th>
-	    <th>Code postale</th>
-	    <th>Ville</th>
-	    <th>Coefficient</th>
-	    <th>Type</th>
-	    <th>Spécialité</th>
-	    <th>Options</th>
-	  </tr>
-
-	  <tr>
-	    <td><input type="checkbox"/></td>
-	    <td></td>
-	    <td></td>
-	    <td></td>
-	    <td></td>
-	    <td></td>
-	    <td></td>
-	    <td></td>
-	    <td></td>
-	  </tr>					  
+	  	 
 	</table>
-</div>	
+</div>
+
+<?php
+    foreach($lesTypePraticiens as $TypePraticiens) //parcours du tableau d'objets récupérés
+    {   
+        $IdTypePrat=$TypePraticiens->getId();           
+        $Libelle=$TypePraticiens ->getLibelle(); 
+       
+    
+?>
+		<tr> 
+        <td><input type="checkbox"/></td>
+        <td><?php echo $IdTypePrat; ?></td>
+        <td><?php echo $Libelle; ?></td>
+        <td></td>
+		</tr>
+
+<?php 
+    } 
+?>	
+
+
