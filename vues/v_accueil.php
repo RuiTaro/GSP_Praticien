cd66da8cf52231be877d2799df01c5c51623beea
lexylievin <32639798+lexylievin@users.noreply.github.com>
2017-11-20 16:36:17 +0100

    pageaccueil

--- vues/v_accueil.php
+++ vues/v_accueil.php
@@ -2,7 +2,8 @@
 <div id="page">
 		<div id="content">
 			<div class="box">
-				<h2>Bienvenue</h2>				
+				<h2>Bienvenue</h2>
+				<p>bonne navigation sur le site d'Alexia, Pavithug et Louis</p>				
 			</div>
 			<div class="box" id="content-box1">
 				
--- vues/v_menuCR.php
+++ vues/v_menuCR.php
@@ -1,6 +1,6 @@
 <body bgcolor="white" text="5599EE">
 
-<div name="haut" style="margin: 2 2 2 2 ;height:6%;"><h1><img src="images/logo.jpg" width="100" height="60"/></div>
+<div name="haut" style="margin: 2 2 2 2 ;height:6%;"><h1><span><a href="accueil.php>"><img src="images/logo.jpg" width="100" height="60"/></a></span></div>
 <div name="gauche" style="float:left;width:100%; background-color:transparent; height:100%;"></div>
 
 	<div id="menu">
@@ -13,7 +13,7 @@
 			</li>
 			<li class="first">
 				<span class="opener"><a href="index.php?uc=GestionPraticiens&action=all" >Praticiens</a></span>
-									
+
 			</li>
 			<li class="first">
 				<span class="opener">Visiteurs</span>
