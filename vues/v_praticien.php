393372d0148ecdd67c94b395de5fcf769b056b7b
lexylievin <32639798+lexylievin@users.noreply.github.com>
2017-11-20 14:55:52 +0100

    m

--- vues/v_praticien.php
+++ vues/v_praticien.php
@@ -79,6 +79,25 @@
 						    <th>Options</th>
 					  	</tr>
 
+					  	<?php
+					  	    foreach($lesSpecialites as $specialite) //parcours du tableau d'objets récupérés
+					  	    {   
+					  	        $idSpec=$specialite->getSpe_Code();           
+					  	        $Libelle=$specialite ->getLibelle(); 
+					  	       
+					  	    
+					  	?>
+					  			<tr> 
+					  	        <td><input type="checkbox"/></td>
+					  	        <td><?php echo $idSpec; ?></td>
+					  	        <td><?php echo $Libelle; ?></td>
+					  	        <td></td>
+					  			</tr>
+
+					  	<?php 
+					  	    } 
+					  	?>
+
 					  	 
 					</table>
 				</div>
