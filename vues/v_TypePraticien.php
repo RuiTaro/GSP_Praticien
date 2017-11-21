dc0346ec18bfc55d791e58c104b208e176d84f2b
lexylievin <32639798+lexylievin@users.noreply.github.com>
2017-11-21 10:34:00 +0100

    btn

--- vues/v_TypePraticien.php
+++ vues/v_TypePraticien.php
@@ -1,5 +1,9 @@
 <div id="tabs-3" class="tab-content">
 	<table class="responstable"> 
+		<div class="btn-ajout">
+		<img src="images/sup.jpg" width="100" height="35" style="height: 35px;border: 1px solid;border-radius: 10px; margin-bottom: 10px"/>	
+		<img src="images/ajouter.jpg" width="100" height="35" style="height: 35px;border: 1px solid;border-radius: 10px; margin-bottom: 10px"/>
+		</div>
 	  	<tr>
 		    <th>Sélection</th>
 		    <th data-th="Driver details"><span>Numéro</span></th>
@@ -20,7 +24,7 @@
         <td><input type="checkbox"/></td>
         <td><?php echo $IdTypePrat; ?></td>
         <td><?php echo $Libelle; ?></td>
-        <td></td>
+        <td><img src="images/btn_rechercher_inactif.gif" style="height: 35px; border: 1px solid; border-radius: 10px; margin: 10px; width: 40px;" /></td>
 		</tr>
 
 <?php 
--- vues/v_specialite.php
+++ vues/v_specialite.php
@@ -1,5 +1,9 @@
 <div id="tabs-2" class="tab-content">
-	<table class="responstable"> 
+	<table class="responstable">
+		<div class="btn-ajout">
+		<img src="images/sup.jpg" width="100" height="35" style="height: 35px;border: 1px solid;border-radius: 10px; margin-bottom: 10px"/>	
+		<img src="images/ajouter.jpg" width="100" height="35" style="height: 35px;border: 1px solid;border-radius: 10px; margin-bottom: 10px"/>
+		</div> 
 	  	<tr>
 		    <th>Sélection</th>
 		    <th data-th="Driver details"><span>Numéro</span></th>
@@ -19,7 +23,7 @@
         <td><input type="checkbox"/></td>
         <td><?php echo $idSpec; ?></td>
         <td><?php echo $Libelle; ?></td>
-        <td></td>
+        <td><img src="images/btn_rechercher_inactif.gif" style="height: 35px; border: 1px solid; border-radius: 10px; margin: 10px; width: 40px;" /></td>
 		</tr>
 
 <?php 
