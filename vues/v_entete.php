0e7201431563ab5e6fd9bf95148f2e4cd46edfff
lexylievin <32639798+lexylievin@users.noreply.github.com>
2017-11-21 11:10:08 +0100

    pagehaut

--- vues/v_entete.php
+++ vues/v_entete.php
@@ -14,14 +14,15 @@
 			<h1><a href="index.php"></a></h1>
 		</div>
 		<div id="slogan">
-			<div name="haut" style="margin: 2 2 2 2 ;height:6%;"><h1><span><a href="index.php?uc=accueil"><img src="images/logo.jpg" width="100" height="60"/></a></span></div>
+			<div name="haut" style="margin: 2 2 2 2 ;height:6%;"><h1><span><a href="index.php?uc=accueil"><img src="images/logo.jpg" width="100" height="60"/></a></span>
 			<!-- <div name="gauche" style="float:left;width:100%; background-color:transparent; height:100%;"></div> -->
-		<?php If (!empty( $_SESSION['connexion'])) // si quelqu'un est connecté
-			{ 
-			echo '<a class="btn" href="index.php?uc=administrer&action=deconnexion">se deconnecter</a>';
-			}else{
-			echo '<a class="btn" href="index.php?uc=administrer&action=connexion"><img src="images/co.png" style="width: 170px;"/></a>';
-			} ?>			
+			<?php If (!empty( $_SESSION['connexion'])) // si quelqu'un est connecté
+				{ 
+				echo '<a class="btn" href="index.php?uc=administrer&action=deconnexion">se deconnecter</a>';
+				}else{
+				echo '<a class="btn" href="index.php?uc=administrer&action=connexion"><img src="images/co.png" style="width: 170px;"/></a>';
+				} ?>
+			</div>			
 		</div>
 	</div>
 
