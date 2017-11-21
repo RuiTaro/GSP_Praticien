2a1430571bb73953e06930ac27a0561455167e0d
lexylievin <32639798+lexylievin@users.noreply.github.com>
2017-11-21 11:38:04 +0100

    humour

--- vues/v_accueil.php
+++ vues/v_accueil.php
@@ -3,7 +3,10 @@
 		<div id="content">
 			<div class="box">
 				<h2>Bienvenue</h2>
-				<p>Bonne navigation sur le site d'Alexia, Pavithug et Louis</p>				
+				<p>
+					Bonne navigation sur le site d'Alexia, Pavithug et Louis
+					<img src="images/drole.jpg"/>
+				</p>				
 			</div>
 			<div class="box" id="content-box1">
 				
