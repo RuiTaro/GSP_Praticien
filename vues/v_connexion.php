d0385dbf0c5aabace53cdfa2a20aa460ce6d4065
lexylievin <32639798+lexylievin@users.noreply.github.com>
2017-11-22 09:30:32 +0100

    mm

--- vues/v_connexion.php
+++ vues/v_connexion.php
@@ -5,7 +5,7 @@
         <div class="form">
           <!-- <img src="https://travelplanet.pro/assets/img/Travel_Planet_blanc.png" alt=""> -->
           <h1>Bienvenue</h1>
-            <!-- <div class="box"> -->
+            <div class="box">
               <h1>Sign in</h1>
               <form action="index.php?uc=administrer&action=verifConnexion" method='POST'>       
                   <div class="input-group">
@@ -20,7 +20,7 @@
 
                   <input type="submit" value="Se connecter">
               </form>
-            <!-- </div> -->
+            </div>
           </div>
         </div>    
       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 910.91 768">
@@ -502,7 +502,7 @@
           input.className = '';
       }
       }</script>
-    </div>
+    <div>
   </div>
   <br class="clearfix" />
 </div> 
