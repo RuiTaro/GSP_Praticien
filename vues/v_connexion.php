5c45fa0b250484805aedb9fa59801a1477da0524
lexylievin <32639798+lexylievin@users.noreply.github.com>
2017-11-22 09:58:16 +0100

    mm

--- vues/v_connexion.php
+++ vues/v_connexion.php
@@ -1,7 +1,28 @@
   <div id="page">
     <div id="content">
       <div class="box">
-         
+         <div class="login">
+        <div class="form">
+          <!-- <img src="https://travelplanet.pro/assets/img/Travel_Planet_blanc.png" alt=""> -->
+          <h1>Bienvenue</h1>
+            <div class="box">
+              <h1>Sign in</h1>
+              <form action="index.php?uc=administrer&action=verifConnexion" method='POST'>       
+                  <div class="input-group">
+                    <input type="login" id="login" name ="login" onblur="checkInput(this)" />
+                    <label for="login">Login</label>
+                  </div>
+
+                  <div class="input-group">
+                    <input type="Password" id="mdp" name ="mdp" onblur="checkInput(this)" />
+                    <label for="mdp">Password</label>
+                  </div>
+
+                  <input type="submit" value="Se connecter">
+              </form>
+            </div>
+          </div>
+        </div> 
       </div>      
     </div>
     <br class="clearfix" />     
