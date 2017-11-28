7157627611aa7e5ffad1c2aefb8082838737cac9
lexylievin <32639798+lexylievin@users.noreply.github.com>
2017-11-28 11:34:46 +0100

    suptp

--- vues/v_praticien.php
+++ vues/v_praticien.php
@@ -1,6 +1,6 @@
 <div id="tabs-1" class="tab-content current">
 	<table class="responstable">
-	<form action ="index.php?uc=GestionPraticiens&action=supprimer" method="POST">
+	<form action ="index.php?uc=GestionPraticiens&action=supprimerTP" method="POST">
 		<div class="btn-ajout">		
 		<input type="submit" value="Supprimer"/><img src="images/sup.jpg" width="100" height="35" style="height: 35px;border: 1px solid;border-radius: 10px; margin-bottom: 10px"/>
 		<a href="index.php?uc=GestionPraticiens&action=ajouter"><img src="images/ajouter.jpg" width="100" height="35" style="height: 35px;border: 1px solid;border-radius: 10px; margin-bottom: 10px"/></a>
